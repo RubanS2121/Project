ccd275e4318ed24050206e529fd8216e80379de4
RubanS2121 <60609974+RubanS2121@users.noreply.github.com>
2020-02-03 18:47:52 +0530

    Add files via upload

---
+++ Main Project/src/main/java/com/main/ClockBean.java
@@ -0,0 +1,63 @@
+package com.main;
+import com.google.appengine.api.datastore.Text;
+
+
+public class ClockBean {
+ 
+	private long start;
+	private long end;
+	private long total;	
+	private String description;
+	private Text descriptionText;
+	private String mailid;
+	public Text getDescriptionText() {
+		return descriptionText;
+	}
+	public void setDescriptionText(Text descriptionText) {
+		this.descriptionText = descriptionText;
+	}
+	public long getStart() {
+		return start;
+	}
+	public void setStart(long start) {
+		this.start = start;
+	}
+	public long getEnd() {
+		return end;
+	}
+	public void setEnd(long l) {
+		this.end = l;
+	}
+	public long getTotal() {
+		return total;
+	}
+	public void setTotal(long total) {
+		this.total = total;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Text toText(String description)
+	{
+		this.descriptionText = new Text(description);
+		return descriptionText;
+	}
+	
+	public String toString(Text descriptionText )
+	{
+		this.description = descriptionText.getValue();
+		return description;
+	}
+	public String getMailid() {
+		return mailid;
+	}
+	public void setMailid(String mailid) {
+		this.mailid = mailid;
+	}
+	
+	
+}
---
+++ Main Project/src/main/java/com/main/Store.java
@@ -0,0 +1,37 @@
+package com.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Store {
+	
+	@RequestMapping(value = "/store", method = RequestMethod.POST)
+	public void store(HttpServletRequest request,HttpServletResponse response)
+	{
+		
+			
+		  ClockBean obj=new ClockBean();
+		  obj.setMailid(obj.getMailid());
+			// System.out.println(obj.getMailid());
+		  obj.setStart(Long.parseLong(String.valueOf(request.getParameter("start"))));
+		  obj.setEnd(Long.parseLong(String.valueOf(request.getParameter("end"))));
+		  obj.setTotal(Long.parseLong(String.valueOf(request.getParameter("tot"))));
+		  obj.setDescriptionText(obj.toText(String.valueOf(request.getParameter("des"))));
+			
+			
+		
+		UserDAO.Store(obj);
+		
+		
+		
+	
+		
+	}
+
+}
